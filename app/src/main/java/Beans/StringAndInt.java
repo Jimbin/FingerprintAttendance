f4c69e97ea62e90f8f5ee892b5af1e5c1df354ca
Jim <1511961346@qq.com>
2017-07-02 00:42:22 +0800

    添加了个人信息页面

---
+++ app/src/main/java/Beans/StringAndInt.java
@@ -0,0 +1,8 @@
+package Beans;
+
+/**
+ * Created by Jim斌 on 2017/7/1.
+ */
+
+public class StringAndInt {
+}
---
+++ app/src/main/java/com/example/wangchang/testbottomnavigationbar/Adapter/ListitemAdaper.java
@@ -0,0 +1,8 @@
+package com.example.wangchang.testbottomnavigationbar.Adapter;
+
+/**
+ * Created by Jim斌 on 2017/7/1.
+ */
+
+public class ListitemAdaper {
+}
